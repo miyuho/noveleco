df95c92e1fcab13493bc9991231faf8104b48743
Ishizawa Miho <krymrihku1220@gmail.com>
2020-10-15 03:53:38 +0000

    アカウント削除機能

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -5,10 +5,23 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
 class ConfigController extends Controller
 {
     public function index()
     {
-        return view('admin.account_config.index');
+        $id = Auth::id();
+        $user = User::find($id);
+        
+        return view('admin.account_config.index', [ 'user'=>$user ]);
+    }
+    
+    public function delete($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect('/');
     }
 }
